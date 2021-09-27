e72a89d3032ebe8d7636e07a6630003614b5da30
Ed Kelly <12986896+edkelly303@users.noreply.github.com>
2021-09-27 21:42:17 +0100

    Add jjant/elm-dict to benchmarks

--- benchmarks/src/DictBenchmarks.elm
+++ benchmarks/src/DictBenchmarks.elm
@@ -1,5 +1,6 @@
 module DictBenchmarks exposing (main)
 
+import AllDict
 import Any.Dict
 import AssocList
 import Benchmark exposing (..)
@@ -34,6 +35,10 @@ asscDict =
 
 eatcDict =
     dict.fromList list
+
+
+jjanDict =
+    AllDict.fromList list
 
 
 updater =
@@ -71,22 +76,29 @@ assc =
     wrap "pzp1997/assoc-list"
 
 
+jjan =
+    wrap "jjant/elm-dict"
+
+
 suite =
     describe "Dict"
         [ rank "fromList"
             [ core (\() -> Dict.fromList list)
             , eatc (\() -> dict.fromList list)
             , assc (\() -> AssocList.fromList list)
+            , jjan (\() -> AllDict.fromList list)
             ]
         , rank "get"
             [ core (\() -> Dict.get 50 coreDict)
             , eatc (\() -> dict.get 50 eatcDict)
             , assc (\() -> AssocList.get 50 asscDict)
+            , jjan (\() -> AllDict.get 50 jjanDict)
             ]
         , rank "insert"
             [ core (\() -> Dict.insert 101 101 coreDict)
             , eatc (\() -> dict.insert 101 101 eatcDict)
             , assc (\() -> AssocList.insert 101 101 asscDict)
+            , jjan (\() -> AllDict.insert 101 101 jjanDict)
             ]
         , rank "update"
             [ core (\() -> Dict.update 50 updater coreDict)
@@ -97,5 +109,6 @@ suite =
             [ core (\() -> Dict.remove 50 coreDict)
             , eatc (\() -> dict.remove 50 eatcDict)
             , assc (\() -> AssocList.remove 50 asscDict)
+            , jjan (\() -> AllDict.remove 50 jjanDict)
             ]
         ]
